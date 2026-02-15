c87efbb05897849aa3488f38d418bd652de99829
kvdh2000 <kishanvdh@gmail.com>
2026-02-15 10:30:03 +0100

    Made it all a switch case

--- Python Lessen etc/HW-w1.py
+++ Python Lessen etc/HW-w1.py
@@ -4,61 +4,62 @@ assign = input("Which assignment do you want to see??")
 try: assign = int(assign)
 except ValueError: print("Das geen getal... pik")
 else:
-	if assign == 1:
-		''' Opdracht 1
-		Tijd omrekenen
-		Maak een variabele aan voor aantal uren
-		Uur naar minuten
-		Minuten naar Seconden
-		Print deze getallen leuk uit
-		'''
+	match assign:
+		case 1:
+			''' Opdracht 1
+			Tijd omrekenen
+			Maak een variabele aan voor aantal uren
+			Uur naar minuten
+			Minuten naar Seconden
+			Print deze getallen leuk uit
+			'''
 
-		hours = input("Enter a number of hours:")
+			hours = input("Enter a number of hours:")
 
-		try: hours = int(hours)
-		except ValueError: print("Das geen getal... pik")
-		else: print("You imput {} hours; which is {} minutes, or {} seconds".format(hours, hours*60, hours*60*60))
+			try: hours = int(hours)
+			except ValueError: print("Das geen getal... pik")
+			else: print("You imput {} hours; which is {} minutes, or {} seconds".format(hours, hours*60, hours*60*60))	
+
+		case 2:
+			''' Opdracht 2
+			Automatisch Tafel Berekenen (e.g. 1 * x, 2 * x, etc t/m 10 * x)
+			Input voor welke tafel
+			Print statements voor elke berekening, en toon de berekening zelf ook
+			Gebruik GEEN for of while, of andere loop, maar aparte regels
+			Of anders.
+			'''
+
+			num = input("Input a table to multiply:")
+
+			try: num = int(num)			
+			except ValueError: print("Das geen getal... pik")
+			else: 
+				table = num
+				i = 1
+
+				print("{} x {} = {}".format(i, table, str(num)))
+				num+=table; i+=1
+				print("{} x {} = {}".format(i, table, str(num)))
+				num+=table; i+=1
+				print("{} x {} = {}".format(i, table, str(num)))
+				num+=table; i+=1
+				print("{} x {} = {}".format(i, table, str(num)))
+				num+=table; i+=1
+				print("{} x {} = {}".format(i, table, str(num)))
+				num+=table; i+=1
+				print("{} x {} = {}".format(i, table, str(num)))
+				num+=table; i+=1
+				print("{} x {} = {}".format(i, table, str(num)))
+				num+=table; i+=1
+				print("{} x {} = {}".format(i, table, str(num)))
+				num+=table; i+=1
+				print("{} x {} = {}".format(i, table, str(num)))
+				num+=table; i+=1
+				print("{} x {} = {}".format(i, table, str(num)))
+		
+				# en met een for loopje, omdat het kan
+				# for i in range (0, 10):
+				# 	print("{} x {} = {}".format(i+1, table, str(num)))
+				# 	num+=table
 	
-	elif assign == 2:
-		''' Opdracht 2
-		Automatisch Tafel Berekenen (e.g. 1 * x, 2 * x, etc t/m 10 * x)
-		Input voor welke tafel
-		Print statements voor elke berekening, en toon de berekening zelf ook
-		Gebruik GEEN for of while, of andere loop, maar aparte regels
-		Of anders.
-		'''
-
-		num = input("Input a table to multiply:")
-
-		try: num = int(num)			
-		except ValueError: print("Das geen getal... pik")
-		else: 
-			table = num
-			i = 1
-
-			print("{} x {} = {}".format(i, table, str(num)))
-			num+=table; i+=1
-			print("{} x {} = {}".format(i, table, str(num)))
-			num+=table; i+=1
-			print("{} x {} = {}".format(i, table, str(num)))
-			num+=table; i+=1
-			print("{} x {} = {}".format(i, table, str(num)))
-			num+=table; i+=1
-			print("{} x {} = {}".format(i, table, str(num)))
-			num+=table; i+=1
-			print("{} x {} = {}".format(i, table, str(num)))
-			num+=table; i+=1
-			print("{} x {} = {}".format(i, table, str(num)))
-			num+=table; i+=1
-			print("{} x {} = {}".format(i, table, str(num)))
-			num+=table; i+=1
-			print("{} x {} = {}".format(i, table, str(num)))
-			num+=table; i+=1
-			print("{} x {} = {}".format(i, table, str(num)))
-   
-			# en met een for loopje, omdat het kan
-			# for i in range (0, 10):
-			# 	print("{} x {} = {}".format(i+1, table, str(num)))
-			# 	num+=table
-	
-	else:	print("Don't have that yet, bub")
+		case _:	print("Don't have that yet, bub")
