61c41eb02cd3fa99b9b2403c44bdef274bcd5e25
kvdh2000 <kishanvdh@gmail.com>
2026-03-03 20:34:58 +0100

    HangEmUp V1
    Also fixed Justin's faulty code....

--- Python Lessen etc/HangEmUp.py
+++ Python Lessen etc/HangEmUp.py
@@ -1,19 +1,47 @@
 import random
+import os
 
 def wordchoice():
-    """ 
-    Maakt een list van woorden uit Woordlijst.txt, en geeft er daar een van terug
+  wordlist = []
 
-    Returns: 
-        Een woord, in string vorm
-    
-    """
-    wordlist = []
-    filepath = "Woordlijst.txt"
-    with open(filepath, "r") as words:
-    #Loopt door alle regels in de woordenlijst https://stackoverflow.com/questions/1323364/in-python-how-to-check-if-a-string-only-contains-certain-characters
-        for line in words:
-            wordlist.append(line.strip())
-    return random.choice(wordlist)
+  here = os.path.dirname(os.path.abspath(__file__))
+  filepath = os.path.join(here, 'Woordlijst.txt')
+  with open(filepath, "r") as words:
+    for line in words:
+      wordlist.append(line.strip())
+  # print(wordlist)
+  return random.choice(wordlist)
 
-print(wordchoice())
+word = wordchoice()
+print(word)
+wordLength = len(word)
+currentState = "_" * wordLength
+currentState = list(currentState)
+guessedLetters = []
+correctGuess = False
+failedGuesses = 0
+
+while True:
+	print("\n")
+	guess = input("Guess a letter or word:")
+	savestate = currentState
+	if len(guess)==1:
+		guessedLetters.append(guess)
+		guessedLetters.sort()
+  
+		for i in range(wordLength):
+			if guess == word[i]:
+				currentState[i]=guess
+				print("You got a letter!!")
+				correctGuess=True
+		if correctGuess==False:
+			print("That letter was not in the word! Womp womp")
+			failedGuesses += 1
+	elif len(guess)>1:
+		print("") # guessing a word??
+
+	print("".join(currentState))
+	print("Guessed letters: {}".format(guessedLetters))
+	# switch case that draws hangman based on failedGuesses
+	# check for win condition
+	correctGuess=False
